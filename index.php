4de9abe3ed2596b0ee541825e1678bc9ea1dbd47
Csr <rasecovarb@gmail.com>
2024-08-15 15:16:21 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+class People {
+    var $firstName;
+    var $lastName;
+
+    function __construct($firstName, $lastName){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    function fullName() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
+$people1 = new People('Cesar', 'Bravo');
+$people2 = new People('Jose', 'Perez');
+//
+echo "{$people1->fullName()} es amigo de {$people2->fullName()}";
