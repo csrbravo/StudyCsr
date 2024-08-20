2d17ea54acf2ff0ec1768f4537dd2af850504207
Csr <rasecovarb@gmail.com>
2024-08-20 10:21:54 -0400

    ajustes clase 3 - 4

--- herencia.php
+++ herencia.php
@@ -2,6 +2,7 @@
 abstract class Unit {
     protected $hp = 50;
     protected $name;
+
     public function __construct($name)
     {
         $this->name = $name;
@@ -12,6 +13,10 @@ abstract class Unit {
         echo "<p>{$this->name} avanza hacia $direction</p>";
     }
 
+    public function unitDie()
+    {
+        echo "<p>{$this->name} muere</p>";
+    }
 
     public function getName()
     {
@@ -20,6 +25,13 @@ abstract class Unit {
 
     abstract function attack($opponent);
 
+    public function takeDamage($damage)
+    {
+        $this->hp -= $damage;
+        if ($this->hp <= 0) {
+            $this->unitDie();
+        }
+    }
 }
 
 class Soldier extends Unit {
@@ -27,7 +39,7 @@ class Soldier extends Unit {
 
     public function attack($opponent)
     {
-        $opponent->hp = $opponent->hp - $this->damage;
+        $opponent->takeDamage($this->damage);
         echo "<p>{$this->name} ataca a {$opponent->getName()}</p>";
         echo "<p>{$opponent->getName()} pierde {$this->damage} puntos de vida</p>";
         echo "<p>{$opponent->getName()} tiene {$opponent->hp} puntos de vida</p>";
@@ -35,12 +47,29 @@ class Soldier extends Unit {
 }
 class Archer extends Unit {
     protected $damage = 20;
+
     public function attack($opponent)
     {
-        $opponent->hp = $opponent->hp - $this->damage;
+        $opponent->takeDamage($this->damage);
         echo "<p>{$this->name} ataca a {$opponent->getName()}</p>";
         echo "<p>{$opponent->getName()} pierde {$this->damage} puntos de vida</p>";
         echo "<p>{$opponent->getName()} tiene {$opponent->hp} puntos de vida</p>";
+        if ($opponent->hp == 0) {
+            echo "<p>{$opponent->getName()} ha muerto</p>";
+            $opponent->unitDie();
+        }elseif ($opponent->hp <0) {
+            echo "<p>{$opponent->getName()} ya no le des mas ya se murio </p>";
+        }
+
+    }
+
+    public function takeDamage($damage)
+    {
+        if (rand(0, 1)) {
+            parent::takeDamage($damage);
+        } else {
+            echo "<p>{$this->name} esquiva el ataque</p>";
+        }
     }
 }
 
@@ -51,6 +80,9 @@ try {
     $Jose = new Soldier('Jose');
     $Jose->move('el sur');
     $Csr->attack($Jose);
+    $Csr->attack($Jose);
+    $Csr->attack($Jose);
+
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage();
 }
