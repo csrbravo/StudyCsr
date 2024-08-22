8de6300cdd95707b476c27c480b0ffd2312ab384
Csr <rasecovarb@gmail.com>
2024-08-22 09:17:52 -0400

    Aplicacion de Polimorfismo-uso de multiples formas una funcion-

--- herencia.php
+++ herencia.php
@@ -7,7 +7,10 @@ abstract class Unit {
     {
         $this->name = $name;
     }
-
+    public function setArmor(Armor $armor = null)
+    {
+        $this->armor = $armor;
+    }
     public function move($direction)
     {
         echo "<p>{$this->name} avanza hacia $direction</p>";
@@ -16,6 +19,7 @@ abstract class Unit {
     public function unitDie()
     {
         echo "<p>{$this->name} muere</p>";
+        exit ("<p>Fin del juego</p>");
     }
 
     public function getName()
@@ -33,16 +37,43 @@ abstract class Unit {
         }
     }
 }
+class Armor {
+    public function absorbDamage(&$damage) {
+        // Example implementation: reduce damage by half
+        $damage /= 2;
+    }
+}
 
 class Soldier extends Unit {
     protected $damage = 10;
+    protected $armor;
 
-    public function attack($opponent)
-    {
-        $opponent->takeDamage($this->damage);
+    public function __construct($name, Armor $armor = null) {
+        $this->armor = $armor;
+        parent::__construct($name);
+    }
+
+    public function setArmor(Armor $armor = null) {
+        $this->armor = $armor;
+    }
+
+    public function attack($opponent) {
+        $damage = $this->damage;
+        if (!is_null($this->armor)) {
+            $this->armor->absorbDamage($damage);
+        }
+        $opponent->takeDamage($damage);
         echo "<p>{$this->name} ataca a {$opponent->getName()}</p>";
-        echo "<p>{$opponent->getName()} pierde {$this->damage} puntos de vida</p>";
+        echo "<p>{$opponent->getName()} pierde {$damage} puntos de vida</p>";
         echo "<p>{$opponent->getName()} tiene {$opponent->hp} puntos de vida</p>";
+    }
+
+    public function takeDamage($damage) {
+        if (rand(0, 1)) {
+            parent::takeDamage($damage);
+        } else {
+            echo "<p>{$this->name} esquiva el ataque</p>";
+        }
     }
 }
 class Archer extends Unit {
@@ -54,13 +85,10 @@ class Archer extends Unit {
         echo "<p>{$this->name} ataca a {$opponent->getName()}</p>";
         echo "<p>{$opponent->getName()} pierde {$this->damage} puntos de vida</p>";
         echo "<p>{$opponent->getName()} tiene {$opponent->hp} puntos de vida</p>";
-        if ($opponent->hp == 0) {
+        if ($opponent->hp <= 0) {
             echo "<p>{$opponent->getName()} ha muerto</p>";
             $opponent->unitDie();
-        }elseif ($opponent->hp <0) {
-            echo "<p>{$opponent->getName()} ya no le des mas ya se murio </p>";
         }
-
     }
 
     public function takeDamage($damage)
@@ -73,16 +101,17 @@ class Archer extends Unit {
     }
 }
 
-
 try {
+    $armor = new Armor();
     $Csr = new Archer('Cesar');
     $Csr->move('el norte');
-    $Jose = new Soldier('Jose');
+    $Jose = new Soldier('Jose', $armor);
     $Jose->move('el sur');
     $Csr->attack($Jose);
     $Csr->attack($Jose);
     $Csr->attack($Jose);
-
+    $Csr->attack($Jose);
+    $Csr->attack($Jose);
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage();
 }
